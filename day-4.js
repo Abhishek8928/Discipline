66b77152dbb14cc0fbf8ed3c5c005d5dabbd4e22
= <abhishekshaxma8356@gmail.com>
2024-05-01 16:21:12 +0530

    day-4 01-May-2024

---
+++ day-4.js
@@ -0,0 +1,84 @@
+// javascript part 2 🚀🚀
+// Date 1-Apr-2024
+
+/*
+
+1. for loop is used to iterate (reapeat) a piece of code
+   for(initilization ; ending condition ; incre/decre){
+       // do something
+   }
+   the loop will executed till the condition is true
+   by adding three statement we can  create a for loop
+   
+2. dry run to execute the code in pen ana paper
+
+3. infinite loop 
+   -> condition missing
+   -> i<= 0  ; i--
+   -> i>=0 ; i++
+
+4. while loop is same as the for loop
+   -> while (condition){
+       // do something
+   }
+   -> this will executed always when he condition is true
+   
+5. break keyword is used to stop the execution of the for loop
+
+6. for of sometime it not works on internet explore
+   -> for(ele of fruits){
+       console.log(ele)
+   }
+   -> fruits has contains an array an each element of array is assigned to the ele and we can direcly access the ele
+
+7. loop with nested array
+   loop inisde loop this is use b asically for the 2d array
+   
+
+
+*/
+
+// const num = 2;
+// const arraNum = [1, 2,2, 3, 4, 56, 7, 7,7, 72, 2];
+
+// for (let i = 0; i < arraNum.length ;i++){
+//     if(arraNum[i] === num){
+//         console.log(arraNum[i],i)
+//         // console.log("removed")
+//         arraNum.splice(i,1)
+//     }
+// }
+
+// let number = 23456;
+// let count = 0;
+// let copy = number;
+
+// while(copy > 0){
+//     count++;
+//     copy = Math.floor(copy/10)
+// }
+
+// console.log(count)
+
+// let num = 145;
+// let sum = 0;
+// let copy = num;
+
+// while(copy > 0){
+//     let remainder = copy % 10;
+//     sum += remainder;
+//     copy = Math.floor(copy / 10);
+// }
+
+// console.log(sum)
+
+// const arr = [23, 34,456, 56, 33, 44, 45, 77];
+// let largest = -1;
+
+// for (let i = 0; i < arr.length; i++){
+//     if(largest < arr[i]){
+//         largest = arr[i]
+//     }
+// }
+
+// console.log(largest)
