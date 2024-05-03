8eeb0830eb673a74e0c1bd3e0504aa86f8814752
= <abhishekshaxma8356@gmail.com>
2024-05-03 20:15:02 +0530

    day 6 03-May-24

---
+++ day-6.js
@@ -0,0 +1,46 @@
+// javascript part 3 🚀🚀
+// Date 3-May-2024
+
+/*
+
+1. function are the piece of code that can executed when we call that function
+   a functuon takes input as the parameter and do some calculation and return a value
+   a function is the standing pillar of any programming language
+   
+   // function definition
+   
+   function function_name(){
+       //do something
+   }
+   
+   // function calling
+   function_name();
+   
+2. function argument are the value that we pass when we call a function. we pass the argument and function receive the parameter 
+
+3. return keyword means now the function work has done and now he will go there where we called or invoked the function because when we call the function is stop execution and function execution has started
+
+4. scope it tell us about where we can access our variable , object ,fuction throught different part of our program 
+
+5. diffrenet types of scope
+   -> block scope is not apply for let and const the variable which we declared inside the block means {} that can access only in particular block example for , if
+   -> global scope any varaiable which is not present inside the function are global scope and can accessible anywhere in the code
+   -> function scope any varaiable that are declared inside the fuction that can be accessible outside the function because the function is invoked the varaiable are created and execution completion the varaiable are deleted from he memory that is why we are not able to access it.
+   -> lexical scope when varaiable are declared outside the function are able to access it 
+   lexical the concept come of nested function a inner function can access anything from the outer function like variable and function also 
+   but the opposite is not true
+
+6. function expression
+   -> whenever we assign a function expression to variable that function are nameless function and can be executed with help of varaiable_name that stored the function expression as the value
+   
+7. Highorder function 
+   -> if the function take one (or) multiple function as the argument then it is higher order function
+   -> if that function also return a function then also it is high order function
+   
+8. method
+  -> whene we create a function inside the object then it is method an operation that can performed over the object
+
+*/
+
+
+
