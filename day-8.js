8eb0880f552c20b59b2823fc198d3c7d60fd7982
= <abhishekshaxma8356@gmail.com>
2024-05-05 23:11:59 +0530

    day-8 05-May-2024

---
+++ day-8.js
@@ -0,0 +1,30 @@
+// javascript part 8 🚀🚀
+// Date 5-May-2024
+
+/*
+
+1. Array method -> forEach , map , filter , every , some ,reduce
+
+2. forEach is use to iterate over the element like for of loop 
+
+3. map is used  to trnsform each element and return a new array with transform value
+
+4. filter is use filter the element of array and return a new array with filtered
+
+5. every it return only true if all element of array return true otherwise false
+
+6. some if return true if any one element is true otherwise false
+
+7. reduces it used to minimize to single value to reduce all element of array to single value
+reducer function and parameter accumulator
+
+8. default parameter if we wanted assign a default value to function parameter 
+always default parameter should be at last
+
+9. array destructring we wanted assign a array value to the distinct varaiable4
+
+10. Rest it provide the function to take n number of parameter and wrap it it with an array
+
+11. Spread operator it use to unpack an we can iterate 
+
+*/
